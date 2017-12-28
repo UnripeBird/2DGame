b35b02b5fd3c3969cd3396213b7dbfc86dd7b746
UnripeBird <hotcoll@naver.com>
2017-12-29 02:20:13 +0900

    오류 수정후 커밋 + 타이틀 씬 추가

---
+++ winApi/winApi/titleScene.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "titleScene.h"
+
+HRESULT titleScene::init(void)
+{
+	IMAGEMANAGER->addImage("titleback", "image\\titleback.bmp", WINSIZEX, WINSIZEY);
+	IMAGEMANAGER->addImage("titlebar", "image\\titlebar.bmp", 713, 68);
+
+	_alpha;
+
+	return S_OK;
+}
+
+void titleScene::release(void)
+{
+}
+
+void titleScene::update(void)
+{
+	_alpha += 3;
+	if (_alpha > 150) _alpha = 0;
+}
+
+void titleScene::render(void)
+{
+	IMAGEMANAGER->render("titleback", getMemDC());
+	IMAGEMANAGER->alphaRender("titlebar", getMemDC(), 44, 366, _alpha);
+}
